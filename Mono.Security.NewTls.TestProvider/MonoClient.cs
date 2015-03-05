4dcc23af75bf57cd399b25f50d2ad6c0ed018215
Martin Baulig <martin.baulig@xamarin.com>
2015-03-05 07:26:40 +0100

    Add MonoConnection and friends; this requires the 'extern alias NewSystemSource'.

---
+++ Mono.Security.NewTls.TestProvider/MonoClient.cs
@@ -0,0 +1,83 @@
+﻿extern alias NewSystemSource;
+
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.Security;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Authentication;
+
+using NewSslPolicyErrors = NewSystemSource::System.Net.Security.SslPolicyErrors;
+using SslProtocols = System.Security.Authentication.SslProtocols;
+using EncryptionPolicy = NewSystemSource::System.Net.Security.EncryptionPolicy;
+
+using Mono.Security.NewTls;
+using Mono.Security.NewTls.TestFramework;
+using Mono.Security.NewTls.TestProvider;
+using Mono.Security.Providers.NewTls;
+
+using SSCX = System.Security.Cryptography.X509Certificates;
+using MX = Mono.Security.X509;
+
+namespace Mono.Security.Instrumentation.Console
+{
+	public class MonoClient : MonoConnection, IClient
+	{
+		new public IClientParameters Parameters {
+			get { return (IClientParameters)base.Parameters; }
+		}
+
+		public MonoClient (IPEndPoint endpoint, IClientParameters parameters)
+			: base (endpoint, parameters)
+		{
+		}
+
+		protected override TlsSettings GetSettings ()
+		{
+			var settings = new TlsSettings ();
+			#if FIXME
+			var monoParams = Parameters as IMonoClientParameters;
+			if (monoParams != null) {
+				settings.ClientCertificateParameters = monoParams.ClientCertificateParameters;
+				settings.Instrumentation = monoParams.ClientInstrumentation;
+			}
+			#endif
+			settings.RequestedCiphers = Parameters.ClientCiphers;
+			return settings;
+		}
+
+		protected override MonoNewTlsStream Start (Socket socket, TlsSettings settings)
+		{
+			Debug ("Connected.");
+
+			var clientCerts = new X509Certificate2Collection ();
+			if (Parameters.ClientCertificate != null) {
+				var clientCert = (ClientCertificate)Parameters.ClientCertificate;
+				clientCerts.Add (clientCert.Certificate);
+			}
+
+			var targetHost = "Hamiller-Tube.local";
+
+			var stream = new NetworkStream (socket);
+
+			return MonoNewTlsStreamFactory.CreateClient (
+				stream, false, RemoteValidationCallback, null, EncryptionPolicy.RequireEncryption, settings,
+				targetHost, clientCerts, SslProtocols.Tls12, false);
+		}
+
+		bool RemoteValidationCallback (object sender, X509Certificate certificate, X509Chain chain, NewSslPolicyErrors errors)
+		{
+			return base.RemoteValidationCallback (sender, certificate, chain, (SslPolicyErrors)errors);
+		}
+
+		bool ClientCertValidationCallback (ClientCertificateParameters certParams, MX.X509Certificate certificate, MX.X509Chain chain, SslPolicyErrors sslPolicyErrors)
+		{
+			return true;
+		}
+	}
+}
---
+++ Mono.Security.NewTls.TestProvider/MonoConnection.cs
@@ -0,0 +1,83 @@
+﻿//
+// MonoConnection.cs
+//
+// Author:
+//       Martin Baulig <martin.baulig@xamarin.com>
+//
+// Copyright (c) 2014 Xamarin Inc. (http://www.xamarin.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+extern alias NewSystemSource;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.Security;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+
+using Mono.Security.NewTls;
+using Mono.Security.Providers.NewTls;
+using Mono.Security.NewTls.TestFramework;
+
+using Xamarin.AsyncTests;
+
+namespace Mono.Security.NewTls.TestProvider
+{
+	public abstract class MonoConnection : DotNetConnection, ICommonConnection
+	{
+		public MonoConnection (IPEndPoint endpoint, IConnectionParameters parameters)
+			: base (endpoint, parameters)
+		{
+		}
+
+		TlsSettings settings;
+		MonoNewTlsStream monoSslStream;
+
+		public override TlsConnectionInfo GetConnectionInfo ()
+		{
+			return settings.ConnectionInfo;
+		}
+
+		protected abstract MonoNewTlsStream Start (Socket socket, TlsSettings settings);
+
+		protected abstract TlsSettings GetSettings ();
+
+		protected sealed override Task<Stream> Start (TestContext ctx, Socket socket, CancellationToken cancellationToken)
+		{
+			return Task.Run<Stream> (() => {
+				settings = GetSettings ();
+				settings.EnableDebugging = Parameters.EnableDebugging;
+				monoSslStream = Start (socket, settings);
+				return monoSslStream;
+			});
+		}
+
+		protected override async Task<bool> TryCleanShutdown (bool waitForReply)
+		{
+			await monoSslStream.Shutdown (waitForReply);
+			return true;
+		}
+	}
+}
+
