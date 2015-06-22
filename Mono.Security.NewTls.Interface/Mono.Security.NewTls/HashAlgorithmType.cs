20441d7e6da390b95e2b73d99ec447dba180c1c7
Martin Baulig <martin.baulig@xamarin.com>
2015-06-22 17:45:37 +0200

    Use special value and add comment to make it clear that it's a Mono extension.

--- Mono.Security.NewTls.Interface/Mono.Security.NewTls/HashAlgorithmType.cs
+++ Mono.Security.NewTls.Interface/Mono.Security.NewTls/HashAlgorithmType.cs
@@ -2,6 +2,7 @@ namespace Mono.Security.NewTls
 {
 	public enum HashAlgorithmType
 	{
+		// These values refer to the @HashAlgorithm enumeration in the TLS 1.2 spec.
 		None	= 0,
 		Md5	= 1,
 		Sha1	= 2,
@@ -9,7 +10,9 @@ namespace Mono.Security.NewTls
 		Sha256	= 4,
 		Sha384	= 5,
 		Sha512	= 6,
-		Md5Sha1	= 7,
-		Unknown	= 255
+		Unknown	= 255,
+
+		// Mono-specific addition, allowing us to reuse it IHashAlgorithm API for TLS 1.0 / 1.1.
+		Md5Sha1	= 254
 	}
 }
