9c1ea7b600ccc561572545a78d0f4df2e3fa3c68
Martin Baulig <martin.baulig@xamarin.com>
2015-07-08 08:51:15 +0200

    Introduce CipherInstrumentTestRunner.

---
+++ Mono.Security.NewTls.TestFramework/Mono.Security.NewTls.TestFramework/CipherInstrumentParameters.cs
@@ -0,0 +1,65 @@
+﻿//
+// CipherInstrumentParameters.cs
+//
+// Author:
+//       Martin Baulig <martin.baulig@xamarin.com>
+//
+// Copyright (c) 2015 Xamarin, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using System.Collections.Generic;
+using Xamarin.AsyncTests;
+using Xamarin.WebTests.Portable;
+using Xamarin.WebTests.ConnectionFramework;
+
+namespace Mono.Security.NewTls.TestFramework
+{
+	public class CipherInstrumentParameters : InstrumentationParameters
+	{
+		public CipherInstrumentType Type {
+			get;
+			private set;
+		}
+
+		public CipherInstrumentParameters (InstrumentationCategory category, CipherInstrumentType type, string identifier, IServerCertificate certificate)
+			: base (category, identifier, certificate)
+		{
+			Type = type;
+		}
+
+		public CipherInstrumentParameters (InstrumentationCategory category, CipherInstrumentType type, ClientParameters clientParameters, ServerParameters serverParameters)
+			: base (category, clientParameters, serverParameters)
+		{
+			Type = type;
+		}
+
+		protected CipherInstrumentParameters (CipherInstrumentParameters other)
+			: base (other)
+		{
+			Type = other.Type;
+		}
+
+		public override ConnectionParameters DeepClone ()
+		{
+			return new CipherInstrumentParameters (this);
+		}
+	}
+}
+
---
+++ Mono.Security.NewTls.TestFramework/Mono.Security.NewTls.TestFramework/CipherInstrumentTestRunner.cs
@@ -0,0 +1,109 @@
+﻿//
+// CipherInstrumentTestRunner.cs
+//
+// Author:
+//       Martin Baulig <martin.baulig@xamarin.com>
+//
+// Copyright (c) 2015 Xamarin, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Xamarin.AsyncTests;
+using Xamarin.AsyncTests.Constraints;
+using Xamarin.WebTests.ConnectionFramework;
+using Xamarin.WebTests.Resources;
+
+namespace Mono.Security.NewTls.TestFramework
+{
+	public class CipherInstrumentTestRunner : InstrumentationTestRunner
+	{
+		new public CipherInstrumentParameters Parameters {
+			get { return (CipherInstrumentParameters)base.Parameters; }
+		}
+
+		public CipherInstrumentType Type {
+			get { return Parameters.Type; }
+		}
+
+		public CipherInstrumentTestRunner (IServer server, IClient client, CipherInstrumentParameters parameters, MonoConnectionFlags flags)
+			: base (server, client, parameters, flags)
+		{
+		}
+
+		public override Instrumentation CreateInstrument (TestContext ctx)
+		{
+			return null;
+		}
+
+		public static IEnumerable<CipherInstrumentParameters> GetParameters (TestContext ctx, InstrumentationCategory category)
+		{
+			switch (category) {
+			default:
+				ctx.AssertFail ("Unsupported instrumentation category: '{0}'.", category);
+				return null;
+			}
+		}
+
+		internal static readonly CipherInstrumentType[] ClientConnectionTypes = {
+		};
+
+		internal static readonly CipherInstrumentType[] ServerConnectionTypes = {
+		};
+
+		internal static readonly CipherInstrumentType[] ConnectionTypes = {
+		};
+
+		internal static readonly CipherInstrumentType[] MartinTestTypes = {
+		};
+
+		static CipherInstrumentParameters CreateParameters (InstrumentationCategory category, CipherInstrumentType type, params object[] args)
+		{
+			var sb = new StringBuilder ();
+			sb.Append (type);
+			foreach (var arg in args) {
+				sb.AppendFormat (":{0}", arg);
+			}
+			var name = sb.ToString ();
+
+			return new CipherInstrumentParameters (category, type, name, ResourceManager.SelfSignedServerCertificate) {
+				ClientCertificateValidator = AcceptAnyCertificate, ServerCertificateValidator = AcceptAnyCertificate,
+				ProtocolVersion = ProtocolVersions.Tls12
+			};
+		}
+
+		static CipherInstrumentParameters Create (TestContext ctx, InstrumentationCategory category, CipherInstrumentType type)
+		{
+			var parameters = CreateParameters (category, type);
+
+			switch (type) {
+			default:
+				ctx.AssertFail ("Unsupported cipher instrument: '{0}'.", type);
+				break;
+			}
+
+			return parameters;
+		}
+	}
+}
+
