bb0d2c7e399fbc26d5e958e53cbddb616bdd4305
Martin Baulig <martin.baulig@xamarin.com>
2015-07-17 16:34:41 +0200

    Updated README.md.
    Please note that the code in this module is not ready yet, it has NOT BEEN SECURITY AUDITED
    AND NO GUARANTEE ABOUT SECURITY OR STABILITY can be made!
    
    You are very welcome to use this module to test the new TLS code, find bugs, problems,
    security issues, anything in it - but do not use it in production environment until it is ready.

--- Mono.Security.NewTls/Mono.Security.NewTls/TlsContext.cs
+++ Mono.Security.NewTls/Mono.Security.NewTls/TlsContext.cs
@@ -12,6 +12,18 @@ namespace Mono.Security.NewTls
 	using Extensions;
 	using Cipher;
 
+	/*
+	 * Please note that the code in this module is not ready yet, it has NOT BEEN SECURITY AUDITED
+	 * AND NO GUARANTEE ABOUT SECURITY OR STABILITY can be made!
+	 *
+	 * You are very welcome to use this module to test the new TLS code, find bugs, problems,
+	 * security issues, anything in it - but do not use it in production environment until it is ready.
+	 *
+	 *
+	 * 'MartinContext' is an empty interface.  It only exists to ensure people read the above message
+	 * prior to building the module.
+	 *
+	 */
 	public class TlsContext : MartinContext, ITlsContext
 	{
 		readonly bool isServer;
--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 Martin's Playground
 ===================
 
+Please note that the code in this module is not ready yet, it has NOT BEEN SECURITY AUDITED
+AND NO GUARANTEE ABOUT SECURITY OR STABILITY can be made!
+
+You are very welcome to use this module to test the new TLS code, find bugs, problems,
+security issues, anything in it - but do not use it in production environment until it is ready.
+
+
+
 For the old version in this file, see the commit history.
 https://github.com/mono/mono-tls/blob/bc4e77604e17a52efe8b0223ccb1998350e1cbc3/README.md
 
