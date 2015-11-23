ebcef1c89ae96c6d227ef93d3e00de89be458160
Martin Baulig <martin.baulig@xamarin.com>
2015-11-23 16:00:56 -0500

    Move ILogger into Mono.Security.NewTls.Interface and add two more methods to it.

--- Mono.Security.NewTls.Interface/Mono.Security.NewTls/ILogger.cs
+++ Mono.Security.NewTls.Interface/Mono.Security.NewTls/ILogger.cs
@@ -4,7 +4,11 @@ namespace Mono.Security.NewTls
 {
 	public interface ILogger
 	{
+		void LogDebug (int level, string format, params object[] args);
+
 		void LogMessage (string format, params object[] args);
+
+		void LogError (string message, Exception error);
 	}
 }
 
